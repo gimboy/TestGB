4ee8ea69c8816cb5a1628ddcd727b55e04f466e1
Timur <gimboy09@mail.ru>
2022-06-14 04:25:33 +0500

    add program

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Написать программу, которая из имеющегося 
+массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. 
+Первоначальный массив можно внести с клавиатуры,
+либо задать на старте выполнения алгоритма.
+*/
+Console.WriteLine("Введите ваш массив и при завершении напишите выход");
+string[] array = new string[100];
+bool hasNext = true;
+
+FillArray(array);
+string[] newArrayS = DelLessTreeCharacters(array);
+ShowArray(newArrayS);
+
+string[] FillArray(string[] array)
+{
+    int i = 0;
+    while (hasNext)
+    {
+        array[i] = Console.ReadLine();
+        if (array[i].Equals("выход")) hasNext = false;
+        i++;
+    }
+    return array;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+string[] DelLessTreeCharacters(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    int k = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] != null && array[i].Length < 4)
+        {
+            newArray[k] = array[i];
+            k++;
+        }
+    }
+    return newArray;
+}
